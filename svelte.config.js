800ddec34d1a524ece805c4d7611b2d836d93ef1
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-02-06 18:32:00 +0100

    change dynamic routes to static routes

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,13 @@ const config = {
 
 	kit: {
 		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
-			strict: false
+			strict: true
 		})
 	}
 };
